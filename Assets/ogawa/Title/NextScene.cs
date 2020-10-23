66f24327d532d39e8ce2179e6e1ee9a21f9bf6a1
name <you@gmail.com>
2020-10-23 13:26:29 +0900

    シーン追加

---
+++ Assets/ogawa/Title/NextScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    [SerializeField] string sceneName = "";
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //シーン遷移
+        if (Input.GetButtonDown("Enter"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
