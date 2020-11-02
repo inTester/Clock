1deac170071c148b6a40588e5ff581f3b2d3ccc2
name <you@gmail.com>
2020-11-02 11:56:34 +0900

    発射音追加

--- Assets/kotani/Script/Bomb_script.cs
+++ Assets/kotani/Script/Bomb_script.cs
@@ -6,6 +6,8 @@ public class Bomb_script : MonoBehaviour
 {
     [SerializeField] private float Limit = default;//爆発までの総時間(秒)
     [SerializeField] private float power = default;//押し出される力
+    [SerializeField] AudioClip soundSE = default;//発射音
+    AudioSource audioSource;
 
     private bool fly;//投げられたか
     private float bornTime;//生成時の時間
@@ -23,6 +25,7 @@ public class Bomb_script : MonoBehaviour
         fly = false;
         g = 1.0f;
         refrect = false;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -63,7 +66,7 @@ public class Bomb_script : MonoBehaviour
             Effect();
             //左右どちらのエリアで爆発したか
             if (this.transform.position.x >= 0) { ExplosionR(1); }
-            else if(this.transform.position.x <= 0) { ExplosionL(1); }
+            else if (this.transform.position.x <= 0) { ExplosionL(1); }
         }
     }
 
@@ -78,12 +81,13 @@ public class Bomb_script : MonoBehaviour
     //当たり判定↓
     void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            if(Input.GetKey("joystick 1 button 5") || Input.GetKeyDown("joystick 2 button 5"))
+            if (Input.GetKey("joystick 1 button 5") || Input.GetKeyDown("joystick 2 button 5"))
             {
                 //ボタン入力で飛ぶ
                 fly = true;
+                audioSource.PlayOneShot(soundSE);
                 //入力を受けた相手の方向取得
                 if (Input.GetKey("joystick 1 button 5"))
                 {
@@ -111,12 +115,12 @@ public class Bomb_script : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         //飛んでる最中プレイヤーとの接触で爆発
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             if (fly)
             {
                 GameObject player_1 = GameObject.Find("Player_1");
-                
+
                 if (collision.gameObject == player_1)
                 {
                     //positionに爆発エフェクト生成
