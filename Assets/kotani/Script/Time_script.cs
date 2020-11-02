ace30ffebc186a8d6806a8cedf1b70f659b4ca73
name <you@gmail.com>
2020-11-02 14:39:01 +0900

    BGM

--- Assets/kotani/Script/Time_script.cs
+++ Assets/kotani/Script/Time_script.cs
@@ -13,26 +13,36 @@ public class Time_script : MonoBehaviour
     [SerializeField] ExplosionCount_script exp_R;
 
     private int startTime; //開始時間
+    public int timeLimit { get; private set; } //残り時間
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
         startTime = (int)Time.time;
+
+        SetTime(); //時間経過の表示
     }
 
     // Update is called once per frame
     void Update()
     {
-        int nowTime = (int)(Time.time - startTime); //経過時間 
-        text.text = (time - nowTime).ToString(); //経過時間から制限時間をひく、
+        SetTime(); //時間経過の表示
 
         //ゲーム終了
-        if ((time - nowTime) <= 0)
+        if ((timeLimit) <= 0)
         {
             SceneManager.sceneLoaded += GameSceneLoaded;
             SceneManager.LoadScene("Result");    // シーン切り替え
         }
+    }
+
+    //時間経過の表示
+    void SetTime()
+    {
+        int elapsedTime = (int)(Time.time - startTime); //経過時間 
+        timeLimit = time - elapsedTime; //経過時間から制限時間をひく、
+        text.text = timeLimit.ToString();
     }
 
     private void GameSceneLoaded(Scene next, LoadSceneMode mode)
