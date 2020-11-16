d4d6153a7a01a974fa24f4ff97dc4c90b00dabc5
name <you@gmail.com>
2020-11-16 10:33:56 +0900

    CPU_ポインター追尾

---
+++ Assets/CPU/CpuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CpuController : MonoBehaviour
+{
+    //[SerializeField] CpuPointer cpuPointerCS = default;
+    [SerializeField] Transform cpuPointer = default;
+    [SerializeField] float SPEED = 1;
+    const float OFFSET = 0.3f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        //追尾
+        Vector3 velocity = Vector3.zero;
+        velocity += cpuPointer.position - transform.position;
+        if (Math.Abs(velocity.x) < OFFSET && Math.Abs(velocity.y) < OFFSET) return; //既に近かったらなし
+        transform.position += velocity.normalized * SPEED;
+    }
+
+
+}
---
+++ Assets/CPU/CpuPointer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CpuPointer : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+
+    ////範囲外なら追尾する
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Debug.Log("et");
+    //    if (collision.gameObject.tag == "pointer")
+    //    {
+    //        isTracking = false;
+    //    }
+    //}
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    Debug.Log("ex");
+    //    if (collision.gameObject.tag == "pointer")
+    //    {
+    //        isTracking = true;
+    //    }
+    //}
+}
