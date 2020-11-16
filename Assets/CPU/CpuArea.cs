08d82d4a6ef03cab046ef72dbfd27abf6a5e5162
name <you@gmail.com>
2020-11-17 01:21:56 +0900

    弾を追いかける

--- Assets/CPU/CpuArea.cs
+++ Assets/CPU/CpuArea.cs
@@ -4,23 +4,72 @@ using UnityEngine;
 
 public class CpuArea : MonoBehaviour
 {
-    int bomCout = 0;
+    [SerializeField] CpuPointer cpuPointer = default;
+    List<GameObject> bombOb = new List<GameObject>();
+    float startTime;
 
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     void Update()
     {
+        if (!ListCheck())
+        {
+            PointerSet();
+        }
+        else
+        {
+            PointerSetBomb();
+        }
+    }
+
+
+
+    //リストが空か
+    bool ListCheck()
+    {
+        if (bombOb.Count > 0)
+        {
+            return true;
+        }
+        return false;
     }
 
     //0だったら一定カウントごとに"場所を変える"を呼び出す
+    void PointerSet()
+    {
+        if (Time.time - startTime > 3.0f)
+        {
+            cpuPointer.ChangePos();
+            startTime = Time.time;
+        }
+    }
+
     //1以上だったら"そのcollitionの場所に行く"を呼び出す
+    void PointerSetBomb()
+    {
+        cpuPointer.ChangePos(bombOb[0].transform.position);
+    }
 
-    //カウントが0だったらtrue
+    //爆弾が入ったらリストに入れる
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Bomb") return;
+        bombOb.Add(collision.gameObject);
+    }
+    //出ていったらリストから消す
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Bomb") return;
 
-
-    //爆弾が入ったらカウントを増やす
-    //出ていったら減らす
+        for (int i = 0; i < bombOb.Count; i++)
+        {
+            if (bombOb[i].name == collision.name)
+            {
+                bombOb.RemoveAt(i);
+            }
+        }
+    }
 }
--- Assets/CPU/CpuPointer.cs
+++ Assets/CPU/CpuPointer.cs
@@ -6,12 +6,23 @@ public class CpuPointer : MonoBehaviour
 {
     void Start()
     {
+
     }
     void Update()
     {
     }
 
     //場所を変えるランダム
+    public void ChangePos()
+    {
+        float x = Random.Range(-1.0f, -8.0f);
+        float y = Random.Range(4.0f, -4.0f);
+        this.transform.position = new Vector3(x, y, 0);
+    }
     //特定の場所に行く
+    public void ChangePos(Vector3 pos)
+    {
+        this.transform.position = pos;
+    }
 
 }
