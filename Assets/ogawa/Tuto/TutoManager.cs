ff3a75b5d072659d3dee4771ec11e019ce72c0bf
name <you@gmail.com>
2020-11-20 15:47:15 +0900

    チュートリアルUI仮置き

---
+++ Assets/ogawa/Tuto/TutoManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class TutoManager : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoPlayer = default;
+    [SerializeField] Text text = default;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
